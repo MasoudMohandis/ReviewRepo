09f727775f46cc6f4b8cf08d6d887544e3036224
MasoudMohandis <132271826+MasoudMohandis@users.noreply.github.com>
2023-05-09 10:13:44 -0400

    Update ReviewClass.java
    MMM

--- src/Review/ReviewClass.java
+++ src/Review/ReviewClass.java
@@ -2,16 +2,5 @@ package Review;
 
 public class ReviewClass {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void test () {
-		
-		System.out.println("This method is on review");
-	}
-	
-	
 	
 }
